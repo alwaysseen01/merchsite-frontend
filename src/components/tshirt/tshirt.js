143b4101289684acf989e9d1fdeef05e7bbab8c7
Igor Ruzhilov <iruzhilov@gmail.com>
2024-02-13 21:55:02 +0600

    development: Add new button and animations while hovering on 'itemBox'

--- src/components/tshirt/tshirt.js
+++ src/components/tshirt/tshirt.js
@@ -6,6 +6,7 @@ const Tshirt = () => {
             <h2 className="itemPrice">20.5$</h2>
             <img className="itemPhoto" src="shirt1.png" alt=""></img>
             <h3 className="itemName">Black T-shirt</h3>
+            <button className="moreDetailsButton">Get more details</button>
         </div>
     );
 };
