f2efa9a71ac2779c2eba5c1fec69377870d3f0c5
Igor Ruzhilov <iruzhilov@gmail.com>
2024-02-13 14:44:58 +0600

    development: Add new 'Tshirt' component with test data

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,9 +1,21 @@
-
+import "./main.css";
+import Tshirt from "../tshirt/tshirt";
 
 const Main = () => {
     return (
         <main className="mainBox">
-
+            <div className="itemsWrapper">
+                {/* FOR LOOPS AND ITEMS FROM FETCH REQUEST */}
+                <Tshirt />
+                <Tshirt />
+                <Tshirt />
+                <Tshirt />
+                <Tshirt />
+                <Tshirt />
+                <Tshirt />
+                <Tshirt />
+                <Tshirt />
+            </div>
         </main>
     );
 };
---
+++ src/components/tshirt/tshirt.js
@@ -0,0 +1,14 @@
+import "./tshirt.css";
+
+const Tshirt = () => {
+    return (
+        <div className="itemBox">
+            <h2 className="itemPrice">20.5$</h2>
+            <img className="itemPhoto" src="shirt1.png" alt=""></img>
+            <h3 className="itemName">Black T-shirt</h3>
+        </div>
+    );
+};
+
+export default Tshirt;
+
