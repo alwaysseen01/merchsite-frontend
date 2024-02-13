1318a4db3a8c2af1b0b41271fa33bc94e1373a6d
Igor Ruzhilov <iruzhilov@gmail.com>
2024-02-13 21:37:35 +0600

    refactoring: Add new component 'tshirtBox' in order to fix routing system and prevent possible bugs with fetch requests in the coming future

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,25 +1,13 @@
 import './app.css';
 import React from "react";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Header from '../header/header';
 import Main from '../main/main';
-
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
-      <Main>
-        <Routes>
-          <Route path="/t-shirts" component="" />
-          <Route path="/sweatshirts" component="" />
-          <Route path="/hoodie" component="" />
-          <Route path="/backpacks" component="" />
-          <Route path="/pants" component="" />
-          <Route path="/shirts" component="" />
-          <Route path="/hats" component="" />
-          <Route path="*" element={<Navigate to="/t-shirts" />} />
-        </Routes>
-      </Main>
+      <Main />
     </BrowserRouter>
   );
 };
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,21 +1,20 @@
 import "./main.css";
-import Tshirt from "../tshirt/tshirt";
+import { Route, Routes, Navigate } from "react-router-dom";
+import TshirtBox from "../tshirtBox/tshirtBox";
 
 const Main = () => {
     return (
         <main className="mainBox">
-            <div className="itemsWrapper">
-                {/* FOR LOOPS AND ITEMS FROM FETCH REQUEST */}
-                <Tshirt />
-                <Tshirt />
-                <Tshirt />
-                <Tshirt />
-                <Tshirt />
-                <Tshirt />
-                <Tshirt />
-                <Tshirt />
-                <Tshirt />
-            </div>
+            <Routes>
+                <Route path="/t-shirts" element={<TshirtBox />} />
+                <Route path="/sweatshirts" element="" />
+                <Route path="/hoodie" element="" />
+                <Route path="/backpacks" element="" />
+                <Route path="/pants" element="" />
+                <Route path="/shirts" element="" />
+                <Route path="/hats" element="" />
+                <Route path="*" element={<Navigate to="/t-shirts" />} />
+            </Routes>
         </main>
     );
 };
